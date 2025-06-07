91360136336a4e3c58973e0d5a6d5cfd2618261f
AdityaRaghav22 <154669994+AdityaRaghav22@users.noreply.github.com>
2025-06-07 15:50:43 +0000

    fixed product.html and app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def dashboard():
   from backend.raw_materials import inventory_raw
   from backend.semi_finished import semi_finished
   from backend.finished import finished_products
@@ -20,11 +20,22 @@ def home():
                          order_count=order_count,
                          active_page='home')
 
+@app.route('/products')
 def inventory():
   from backend.raw_materials import inventory_raw
   from backend.semi_finished import semi_finished
   from backend.finished import finished_products
-  return render_template('products.html', inventory_raw=inventory_raw, semi_finished=semi_finished, finished_products=finished_products, active_page='inventory')
+  from backend.sales_orders import sales_order 
+  raw_count = len(inventory_raw)
+  semi_count = len(semi_finished)
+  finished_count = len(finished_products)
+  order_count = list(sales_order.items())[-5:]
+  return render_template('products.html',
+                         raw_count = raw_count,
+                         semi_count = semi_count,
+                         finished_count = finished_count,
+                         order_count = order_count,
+                         active_page='inventory')
 
 if __name__ == '__main__':
   app.run(host = "0.0.0.0", debug=True)
